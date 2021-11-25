a6e9847201756a4e84e7e9dac03d5a675ab827cd
jihye <dg60866090@gmail.com>
2021-11-25 15:19:11 +0900

    feat : Print 'buzz' if i is times of 5
    TODO
    - print fizzbuzz if i is times of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,7 @@ def do_fizzbuzz():
   """
   Do fizzbuzz with for and if 
   While i goes up 1 to 15, 
-  print 'fizz' if i is time of 3 
+  print 'fizz' if i is time of 3 (done) 
   print 'buzz' if i is times of 5
   print 'fizzbuzz' if i is times of 15
   else, print i (string)
@@ -13,6 +13,8 @@ def do_fizzbuzz():
    for i in ragne(1,15+1):
     if i%3==0:
         print('fizz')
+    elif i%5==0:
+        print('buzz')
     else:
        print ('{}'.format(i))
    return True
